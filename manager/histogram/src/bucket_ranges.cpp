efce651bbd3e30f1b05033569ce352dd97f81c9c
Gilmour <liaohanwen4@huawei.com>
2026-04-21 10:49:41 +0800

    update: 更新文件 bucket_ranges.cpp
    Signed-off-by: Gilmour <liaohanwen4@huawei.com>

--- manager/histogram/src/bucket_ranges.cpp
+++ manager/histogram/src/bucket_ranges.cpp
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
- 
+
 #include "bucket_ranges.h"
 
 #include <algorithm>
@@ -57,7 +57,6 @@ void BucketRanges::InitializeLinearBuckets(int32_t minimum, int32_t maximum, siz
     double bucket_width = (divisor == 0)
         ? 0
         : static_cast<double>(maximum - minimum) / divisor;
-
     ranges_[0] = 0;
 
     for (size_t i = 1; i < bucket_count - 1; i++) {
@@ -68,9 +67,6 @@ void BucketRanges::InitializeLinearBuckets(int32_t minimum, int32_t maximum, siz
     ranges_[bucket_count] = INT_MAX;
 }
 
-/**
- * Exponential Buckets:
- */
 void BucketRanges::InitializeExponentialBuckets(int32_t minimum, int32_t maximum, size_t bucket_count)
 {
     if (bucket_count < MIN_EXP_BUCKET_COUNT ||
@@ -89,27 +85,20 @@ void BucketRanges::InitializeExponentialBuckets(int32_t minimum, int32_t maximum
     for (size_t bucket_index = SECOND_BUCKET_INDEX;
          bucket_index < bucket_count;
          ++bucket_index) {
-
         double log_current = std::log(static_cast<double>(current));
         size_t remaining_buckets = bucket_count - bucket_index;
 
         double log_ratio = (log_max - log_current) /
             static_cast<double>(remaining_buckets);
-
         double log_next = log_current + log_ratio;
-
         int32_t next =
             static_cast<int32_t>(std::round(std::exp(log_next)));
-
         current = (next > current) ? next : (current + 1);
-
         if (current > maximum) {
             current = maximum;
         }
-
         ranges_[bucket_index] = current;
     }
-
     ranges_[bucket_count - 1] = maximum;
     ranges_[bucket_count] = INT_MAX;
 }
@@ -119,17 +108,13 @@ int BucketRanges::GetBucketIndex(int32_t value) const
     if (ranges_.empty()) {
         return -1;
     }
-
     auto it = std::upper_bound(ranges_.begin(), ranges_.end(), value);
-
     int32_t index =
         static_cast<int32_t>(std::distance(ranges_.begin(), it)) - 1;
-
     if (index < 0 ||
         index >= static_cast<int32_t>(ranges_.size() - 1)) {
         return -1;
     }
-
     return index;
 }
 
